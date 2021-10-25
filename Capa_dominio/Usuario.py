f43810bc483ff1ed05a741802bb186236016e4dc
jaquelinepv <jaquelinepvla@gmail.com>
2021-10-25 17:03:09 -0600

    Se agrego la clase acceso

--- Capa_dominio/Usuario.py
+++ Capa_dominio/Usuario.py
@@ -1,13 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, SelectField, SubmitField, PasswordField
+from wtforms import Form, StringField, SelectField, SubmitField, PasswordField, BooleanField
 from wtforms import validators
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import InputRequired, DataRequired, Email, Length, EqualTo
 import email_validator
 
 class registro(FlaskForm):
-    nombre = StringField('Nombre', validators= [DataRequired('Favor de insertar su nombre completo'), Length(min=5, max=50)])
-    email = StringField('Email', validators=[DataRequired('Favor de incertar un correo valido'), Email()])
-    usuario = StringField('Nombre de usuario', validators=[DataRequired('Favor de insertar un nombre de usuario valido'), Length(min=5, max=20)])
-    contraseña = PasswordField('contraseña', validators =[DataRequired()])
-    confirmar = PasswordField('Confirmar contraseña: ', validators=[DataRequired(), EqualTo('contraseña', message='La contraseñas no coinciden')])
+    nombre = StringField('Nombre', validators= [InputRequired('Favor de insertar su nombre completo'), Length(min=5, max=50)])
+    email = StringField('Email', validators=[InputRequired(), Email('Favor de incertar un correo valido'), Length(min=5, max=20)])
+    usuario = StringField('Nombre de usuario', validators=[InputRequired('Favor de insertar un nombre de usuario valido'), Length(min=5, max=20)])
+    contraseña = PasswordField('Contraseña', validators =[InputRequired(), Length(min=8, max=20)])
     enviar = SubmitField('Registrarse')
+
+class acceso(FlaskForm):
+    usuario = StringField('Nombre de usuario', validators=[InputRequired('Favor de insertar un nombre de usuario valido'), Length(min=5, max=20)])
+    contraseña = PasswordField('contraseña', validators =[InputRequired(), Length(min=8, max=20)])
+    recordarme= BooleanField('Recordarme')
+    enviar = SubmitField('Registrarse')
+    
+    
