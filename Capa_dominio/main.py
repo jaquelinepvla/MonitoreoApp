a023a051f600a93133b8f2e786fc398a2040f5a7
jaquelinepv <jaquelinepvla@gmail.com>
2021-10-25 17:04:00 -0600

    Conexión a la base de datos

--- Capa_dominio/main.py
+++ Capa_dominio/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 import psycopg2
 from Usuario import registro 
-from psycopg2 import randint
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xa6\x8b\xafF\xee\x81\xaa\x0e\xb8/\xd4H\xdb\xff\x9b\x19g+sM\x8dQ\xda\x05'
 @app.route("/", methods=['POST', 'GET'])
@@ -16,9 +17,9 @@ def registrar():
 		Email = form.email.data
 		Usuario = form.usuario.data
 		Contraseña = form.contraseña.data
-		Confirmar = form.confirmar.data
-		print(Nombre, Email, Usuario, Contraseña, Confirmar)
-		form.nombre.data, Email = form.email.data, form.usuario.data, form.contraseña.data = "", "", "", ""
+	
+		print(Nombre, Email, Usuario, Contraseña)
+		
 		#Enviar información a la base de datos sin el contexto de la app
 		conn = psycopg2.connect(
 			host = "localhost",
@@ -28,14 +29,11 @@ def registrar():
 			password= "12345"
 		)
 		cursor = conn.cursor()
-		cursor.execute('''
-		INTERT INTO Parámetros(ID, Email, Usuario, Contraseña)
-			VALUES(%S, %S, %S, %S)
-		''',(randint(0,1000000), Email, Nombre, Usuario, Confirmar))
-		cursor.commit()
+		cursor.execute("INSERT INTO usuarios (email, nombre, usuario, contrasena) VALUES('{0}', '{1}', '{2}', '{3}')".format(Email, Nombre, Usuario, Contraseña))
+		conn.commit()
 		cursor.close()
 		conn.close()
-
+	
 		return render_template('Index.html', form=form)
 	return render_template('Registro.html', form=form)
 
